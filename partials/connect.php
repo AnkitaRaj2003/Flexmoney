200f7a68f9affdef0a8103575ebb170d7d7f4742
Ankita meena <104414458+AnkitaRaj2003@users.noreply.github.com>
2023-12-19 19:51:25 +0530

    Add files via upload

---
+++ partials/connect.php
@@ -0,0 +1,8 @@
+<?php
+    $server = 'localhost';
+    $username = 'root';
+    $password = '';
+    $database = 'flexmoney';
+
+    $conn = mysqli_connect($server,$username,$password,$database);
+?>
